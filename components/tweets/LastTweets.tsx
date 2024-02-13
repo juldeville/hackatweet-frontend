75c3b0d31dbb84887f118012fa70c735719253b0
Julien Deville <j.deville@protonmail.com>
2024-02-13 15:37:47 +0100

    added useEffect to display all tweets on LastTweets mount

--- components/tweets/LastTweets.tsx
+++ components/tweets/LastTweets.tsx
@@ -1,6 +1,9 @@
 import Tweet from "./Tweet";
+import { useState } from "react";
 
 export default function LastTweets() {
+  const [tweetData, setTweetData] = useState([]);
+
   return (
     <div>
       <Tweet />
